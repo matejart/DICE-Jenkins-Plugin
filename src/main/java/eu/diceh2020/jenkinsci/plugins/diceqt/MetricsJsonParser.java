71532971535f08e5c3ff391403bb4399128b4c7d
Matej Artač <matej.artac@xlab.si>
2016-10-18 15:09:14 +0200

    Add json file parser for parsing quality metrics
    The metrics parser saves simple flat key-value JSON files. This
    will be saved in each build's Action and displayed as chart
    or table.

---
+++ src/main/java/eu/diceh2020/jenkinsci/plugins/diceqt/MetricsJsonParser.java
@@ -0,0 +1,88 @@
+package eu.diceh2020.jenkinsci.plugins.diceqt;
+
+/*-
+ * #%L
+ * DICE Jenkins plug-in
+ * %%
+ * Copyright (C) 2016 XLAB d.o.o.
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import org.json.*;
+
+import hudson.FilePath;
+
+/**
+ * Class for parsing metrics data.
+ * 
+ * @author matej.artac (at) xlab.si
+ *
+ */
+public class MetricsJsonParser {
+	
+	/**
+	 * Parse the file indicated by the filePath parameter from a JSON
+	 * file. It expects a flat structure of key-value pairs with the
+	 * values being a numerical value. 
+	 * 
+	 * @param filePath Describes location of the results file to be
+	 * parsed.
+	 * @return A dictionary of metric names and their values. If the
+	 * file doesn't exist, the return value is null.
+	 * @throws InterruptedException Thrown during processing 
+	 * remoting operations
+	 * @throws IOException 
+	 */
+	public static Dictionary<String, Number> parse(FilePath filePath) 
+			throws IOException, InterruptedException {
+
+		if (!filePath.exists()) {
+			return null;
+		}
+		
+		String jsonSource = filePath.readToString();
+	
+		Dictionary<String, Number> retval = 
+				new Hashtable<String, Number>();
+		
+		if (jsonSource.trim().isEmpty()) {
+			// return an empty dictionary
+			return retval;
+		}
+		
+		JSONObject json = new JSONObject(jsonSource);
+		Iterator<String> k = json.keys();
+		while (k.hasNext()) { 
+			String metric = k.next();
+			Double value = json.getDouble(metric);
+			
+			retval.put(metric, value);
+		}
+
+		return retval;
+	}
+}
---
+++ src/test/java/eu/diceh2020/jenkinsci/plugins/diceqt/MetricsJsonParserTest.java
@@ -0,0 +1,158 @@
+package eu.diceh2020.jenkinsci.plugins.diceqt;
+
+/*-
+ * #%L
+ * DICE Jenkins plug-in
+ * %%
+ * Copyright (C) 2016 XLAB d.o.o.
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.json.JSONException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import hudson.FilePath;
+
+public class MetricsJsonParserTest {
+
+	@Test
+	public void testParseFileNotExists() throws Exception {
+		File localPath = new File("/tmp/file-not-exists-json");
+		if (localPath.exists())
+			throw new Exception(String.format(
+					"Test implementation problem: '%s' should not exist.",
+					localPath.getAbsolutePath()));
+		FilePath pathNoFile = new FilePath(localPath);
+		
+		Dictionary<String, Number> results =
+				MetricsJsonParser.parse(pathNoFile);
+		
+		assertNull(results);
+	}
+	
+	@Test
+	public void testParseEmpty() throws Exception {
+		File tmpFile = File.createTempFile("diceci-", ".json");
+		tmpFile.deleteOnExit();
+
+		FilePath pathEmptyFile = new FilePath(tmpFile);
+		
+		Dictionary<String, Number> results =
+				MetricsJsonParser.parse(pathEmptyFile);
+		
+		assertNotNull(results);
+		assertEquals(0, results.size());
+	}
+
+	@Test
+	public void testParseSingleEntry()
+			throws IOException, InterruptedException {
+		
+		String fileContent = "{'latency': 123.55}";
+		Hashtable<String, Number>  expected = new Hashtable<String, Number>();
+		expected.put("latency", 123.55);
+		
+		FilePath pathSingleEntry = this.createTemporaryFile(fileContent);
+
+		Dictionary<String, Number> results =
+				MetricsJsonParser.parse(pathSingleEntry);
+		
+		assertNotNull(results);
+		assertEquals(1, results.size());
+		assertEquals(expected, results);
+	}
+	
+	@Test
+	public void testParseMultipleEntries()
+			throws IOException, InterruptedException {
+		
+		String fileContent = "{"
+				+ "'latency': 123.55,"
+				+ "'throughput': 0.0031,"
+				+ "'time': 500"
+				+ "}";
+		
+		Hashtable<String, Number>  expected = new Hashtable<String, Number>();
+		expected.put("latency", 123.55);
+		expected.put("throughput", 0.0031);
+		expected.put("time", 500.0);
+		
+		FilePath pathMultipleEntries = this.createTemporaryFile(fileContent);
+
+		Dictionary<String, Number> results =
+				MetricsJsonParser.parse(pathMultipleEntries);
+		
+		assertNotNull(results);
+		assertEquals(expected.size(), results.size());
+		assertEquals(expected, results);
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testParseNonFlatEntres()
+			throws IOException, InterruptedException {
+
+		String fileContent = "{"
+				+ "'latency': 123.55,"
+				+ "'throughput': 0.0031,"
+				+ "'time': 500,"
+				+ "'bla': { 'subkey1': 200 }"
+				+ "}";
+		
+		FilePath pathBadInput = this.createTemporaryFile(fileContent);
+		
+		thrown.expect(JSONException.class);
+		MetricsJsonParser.parse(pathBadInput);
+	}
+	
+	private FilePath createTemporaryFile(String fileContent)
+			throws IOException {
+		File tmpFile = File.createTempFile("diceci-", ".json");
+		tmpFile.deleteOnExit();
+		
+		FileOutputStream fout = new FileOutputStream(tmpFile);
+		OutputStreamWriter stream = new OutputStreamWriter(fout);
+		
+		stream.write(fileContent);
+		
+		stream.close();
+		fout.close();
+		
+		FilePath retval = new FilePath(tmpFile);
+		
+		return retval;
+	}
+}
